e312e43273d6ad26c6438c467e9fde77878dea7e
jahongir <jaxobgir799@gmail.com>
2021-08-25 20:21:43 +0500

    Initial Commit

---
+++ SwiftUiTesty/SwiftUiTestyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiTestyApp.swift
+//  SwiftUiTesty
+//
+//  Created by Jahongir on 25/08/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiTestyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
